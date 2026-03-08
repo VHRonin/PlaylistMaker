9d1d98bcc48f02f1f55d4a4603520dbd49870fcc
VHRonin <boy44127@gmail.com>
2026-03-08 22:58:36 +0300

    Спринт 9 задание 3

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,12 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,43 @@ class SearchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearButton = findViewById<ImageButton>(R.id.clearButton)
+        val searchBack = findViewById<ImageView>(R.id.searchBack)
+
+        searchBack.setOnClickListener { finish() }
+
+        clearButton.setOnClickListener { searchEditText.setText("") }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                clearButton.visibility = buttonVisibility(s)
+            }
+
+        }
+
+        searchEditText.addTextChangedListener(searchTextWatcher)
     }
+
+    private fun buttonVisibility(s: CharSequence?) = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+
 }
